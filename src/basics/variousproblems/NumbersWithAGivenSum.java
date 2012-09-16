2273057e4c3546feb5c6187be83aa7279b63d440
Vedrana <vedrana.jankovic@gmail.com>
2012-09-16 22:49:23 +0200

    Find two numbers whose sum is s

---
+++ src/basics/variousproblems/NumbersWithAGivenSum.java
@@ -0,0 +1,35 @@
+package basics.variousproblems;
+
+/**
+ * Given an increasingly sorted array and a number s,
+ * find two numbers whose sum is s. If there are multiple
+ * pairs with sum s, just output any one of them.
+ */
+public class NumbersWithAGivenSum {
+
+	public boolean findNumbers(int[] array, int sum) {
+		int left = 0;
+		int right = array.length - 1;
+		
+		while (left < right) {
+			int candidateSum = array[left] + array[right];
+			if (candidateSum == sum) {
+				System.out.println(array[left] + " + " + array[right] + " == " + sum);
+				return true;
+			} else if (candidateSum < sum) {
+				left++;
+			} else {
+				right--;
+			}
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		NumbersWithAGivenSum givenSum = new NumbersWithAGivenSum();
+		int[] array = new int[] {1, 2, 4, 7, 11, 15};
+		int wantedSum = 15;
+		givenSum.findNumbers(array, wantedSum);
+	}
+
+}
