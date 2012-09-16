f1a7fa6f01b4f0af54a6d194273938b00256a636
Vedrana <vedrana.jankovic@gmail.com>
2012-09-16 18:00:41 +0200

    Check if a number is a palindrome

---
+++ src/basics/variousproblems/NumberInversion.java
@@ -0,0 +1,33 @@
+package basics.variousproblems;
+
+// Given a positive integer in base 10, find its reverse,
+// i.e. make the least significant digits the most significant
+// and the opposite
+public class NumberInversion {
+
+	public int invert(int number) {
+		int invert = 0;
+		int num = number;
+		
+		while (num > 0) {
+			invert = invert * 10 + num%10;
+			num /= 10;
+		}
+		return invert;
+	}
+	
+	/**
+	 * Check if a number is a palindrome without
+	 * transforming it to string first 
+	 */
+	public boolean isPalindrome(int number) {
+		return number == invert(number);
+	}
+	
+	public static void main(String[] args) {
+		NumberInversion numInv = new NumberInversion();
+		System.out.println("123 is a palindrome: " + numInv.isPalindrome(123));
+		System.out.println("12321 is a palindrome: " + numInv.isPalindrome(12321));
+	}
+
+}
